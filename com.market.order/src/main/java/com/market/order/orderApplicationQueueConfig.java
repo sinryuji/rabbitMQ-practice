e8e0a38eac7ba32de262c435ed581bcae1c6feed
sinryuji <siler2474@gmail.com>
2025-03-07 21:17:14 +0900

    rename

--- com.market.order/src/main/java/com/market/order/orderApplicationQueueConfig.java
+++ com.market.order/src/main/java/com/market/order/orderApplicationQueueConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class orderApplicationQueueConfig {
+public class OrderApplicationQueueConfig {
 
     @Value("${message.exchange}")
     private String exchange;
